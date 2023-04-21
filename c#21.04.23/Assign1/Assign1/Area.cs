b4a6875782a5cb7377596302b87fa4747424dcc6
Saloni Kumari <ssalonikkr@gmail.com>
2023-04-21 17:07:17 +0530

    Assignment update on 21.04.2023

---
+++ c#21.04.23/Assign1/Assign1/Area.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace PolymorphismApp
+{
+    abstract class Areas
+    {
+        public abstract double area();
+    }
+
+    class Circle : Areas
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+
+        }
+
+        public override double area()
+        {
+            Console.WriteLine(" Area of the circle :");
+            return (3.14159265359 * radius * radius);
+        }
+    }
+
+    class Square : Areas
+    {
+        private double side;
+
+        public Square(double s)
+        {
+            side = s;
+
+        }
+
+        public override double area()
+        {
+            Console.WriteLine("Area of the Square :");
+            return (side * side);
+        }
+    }
+    class Rectangle : Areas
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(int a = 0, int b = 0)
+        {
+            length = a;
+            width = b;
+        }
+
+        public override double area()
+        {
+            Console.WriteLine("Rectangle class area :");
+            return (width * length);
+        }
+    }
+
+    class Area
+    {
+
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            double c1 = c.area();
+            Console.WriteLine("Area: {0}", c1);
+
+
+
+            Square s = new Square(10);
+            double s1 = s.area();
+            Console.WriteLine("Area: {0}", s1);
+
+
+
+            Rectangle r = new Rectangle(20, 5);
+            double a = r.area();
+            Console.WriteLine("Area: {0}", a);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ c#21.04.23/Emp2/Emp2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+
+public class Employee
+{
+
+    
+    public float amount = 40000;
+  
+    public void numberofsales1()
+    {
+        Console.WriteLine("Invalid number of Sale");
+    }
+
+    public void numberofsale()
+    {
+
+        Console.WriteLine("Total Sale per Month:");
+        Console.WriteLine("1st month of sale:10");
+        Console.WriteLine("2nd month of sale:20");
+        Console.WriteLine("3rd month of sale:30");
+    }
+}
+
+public class SalesAssociate : Employee
+{
+
+    
+    double bonus;
+    public void salesbonus()
+    {
+        
+
+        
+        string val;
+        Console.WriteLine("Enter the number of sales:");
+        val = Console.ReadLine();
+
+        int nos;
+        nos = Convert.ToInt32(val);
+
+
+        if (nos == 10)
+        {
+            Console.WriteLine("Total number of sales :" +nos);
+            bonus = (5 * amount) / 100;
+            Console.WriteLine("Bonus:"+bonus);
+        }
+        if (nos == 20)
+        {
+            Console.WriteLine("Total number of sales :" +nos);
+            bonus = (10 * amount) / 100;
+            Console.WriteLine("Bonus:" +bonus);
+
+        }
+        if (nos == 30)
+        {
+            Console.WriteLine(" Total number of sales :" + nos);
+            bonus = (20 * amount) / 100;
+           
+            Console.WriteLine("Bonus:"+bonus);
+
+        }
+        if(nos !=10 && nos !=20 && nos !=30) 
+        {
+            numberofsales1();
+        }
+    }
+
+
+}
+class TestInheritance
+{
+    public static void Main(string[] args)
+    {
+        SalesAssociate b = new SalesAssociate();
+        b.numberofsale();
+        b.salesbonus();
+       
+       
+        
+
+
+    }
+}
